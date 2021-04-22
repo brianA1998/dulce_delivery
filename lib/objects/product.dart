e8be1f58f4cad3629e417d0704af35f05d941776
Brian <brian.alaniz@hotmail.com>
2021-04-22 20:22:22 -0300

    Desarrollando la clase de Products

--- lib/objects/product.dart
+++ lib/objects/product.dart
@@ -1,1 +1,27 @@
-class Product {}
+import 'package:firebase_database/firebase_database.dart';
+
+class Product {
+  Product(this._idProduct, this._nameProduct, this._priceProduct,
+      this._cuantityProduct, this._descriptionProduct);
+
+  String _idProduct;
+  String _nameProduct;
+  int _priceProduct;
+  int _cuantityProduct;
+  String _descriptionProduct;
+
+  Product.map(dynamic obj) {
+    this._nameProduct = obj['nameProduct'];
+    this._priceProduct = obj['priceProduct'];
+    this._cuantityProduct = obj['cuantityProduct'];
+    this._descriptionProduct = obj['descriptionProduct'];
+  }
+
+  String get id => _idProduct;
+  String get name => _nameProduct;
+  int get price => _priceProduct;
+  int get cuantity => _cuantityProduct;
+  String get description => _descriptionProduct;
+
+  Product.fromSnapShot(DataSnapshot snapshot) {}
+}
